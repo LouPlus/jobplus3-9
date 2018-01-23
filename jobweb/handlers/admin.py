9af01ce82d29667d7e9d0aa2957ca39e9091e9f0
peiyuy <peiyuyi1103@163.com>
2018-01-23 00:38:32 -0800

    添加 admin部分代码 (#29)
    * Create decorators.py
    
    * Delete decorators.py
    
    * Create decorators.py
    
    * Update admin.py
    
    * 添加 管理员控制台

--- jobweb/handlers/admin.py
+++ jobweb/handlers/admin.py
@@ -1,3 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from jobweb.decorators import admin_required
 
-admin = Blueprint('admin', __name__, url_prefix='/')
+admin = Blueprint('admin', __name__, url_prefix='/admin')
+
+# admin的首页
+@admin.route('/')
+@admin_required
+def admin_index():
+  return render_template('admin/index.html')
