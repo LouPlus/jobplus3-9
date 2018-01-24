44bc938cd0d4ca2988461173859d0865379f5bd0
peiyuy <peiyuyi1103@163.com>
2018-01-23 23:48:31 -0800

    Update admin.py

--- jobweb/handlers/admin.py
+++ jobweb/handlers/admin.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, current_app
 from jobweb.decorators import admin_required
+from jobweb.models import User
+from jobweb.forms import UserRegisterForm
 
 admin = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -8,3 +10,25 @@ admin = Blueprint('admin', __name__, url_prefix='/admin')
 @admin_required
 def admin_index():
   return render_template('admin/index.html')
+
+# 实现用户管理
+@admin.route('/users')
+@admin_required
+def users():
+  page = request.args.get('page', default=1, type=int)
+  pagination = User.query.paginate(
+    page=page,
+    per_page=current_app.config['ADMIN_PER_PAGE'],
+    error_out = False
+  )
+  return render_template('admin/users.html', pagination=pagination)
+
+@admin.route('/users/adduser', methods=['GET', 'POST'])
+@admin_required
+def create_user():
+  user = UserForm()
+  if form.validate_on_submit():
+    form.create_user()
+    flash('create user success', 'success') #添加求职者成功
+    return redirect(url_for('admin.users'))
+  return render_template('admin/user/adduser.html', form=form)
