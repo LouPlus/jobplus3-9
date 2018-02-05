c535df767305fb3d2e54cab7c9f16ab2828fbb59
wmingjun94 <wmingjun94@gmail.com>
2018-02-05 14:16:09 +0800

    Production round 1 bug fix

--- manage.py
+++ manage.py
@@ -3,4 +3,6 @@ from jobweb.app import create_app
 app = create_app('development')
 
 if __name__ =='__main__':
+    from werkzeug.contrib.fixers import ProxyFix
+    app.wsgi_app=ProxyFix(app.wsgi_app)
     app.run()
